743c92d9dcdc01dcfa21f2a7beab5672e3c0e11a
marvin evins <maevins@cps.edu>
2020-02-23 15:27:38 -0600

    created url dynamic

--- craigbot.py
+++ craigbot.py
@@ -7,3 +7,26 @@ from selenium.common.exceptions import TimeoutException
 
 from bs4 import BeautifulSoup
 import urllib.request
+
+class CraigslistScraper(object):
+    def __init__(self, location, postal, max_pirce, radius):
+        self.location = location
+        self.postal = postal
+        self.max_price = max_price
+        self.radius = radius
+
+        #search_distance=5&postal=60649&max_price=500
+
+        self.url = f"https://{location}.craigslist.org/search/sss?/search_distance={radius}&postal={postal}&max_price={max_price}"
+
+    def test(self):
+        print(self.url)
+
+location = "sfbay"
+postal="94201"
+max_price = "500"
+radius = "5"
+
+
+scraper = CraigslistScraper(location, postal, max_price, radius)
+scraper.test()
